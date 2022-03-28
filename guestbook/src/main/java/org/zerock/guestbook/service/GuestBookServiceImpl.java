43315d86e1296851d5ead9ff664d35e503ba1f34
Beginner <dkstmdwn17@naver.com>
2022-03-29 00:17:57 +0900

    삽입, 목록 처리 관련 메서드 작성
    삽입, 목록 처리 관련 메서드 작성

---
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestBookServiceImpl.java
@@ -0,0 +1,49 @@
+package org.zerock.guestbook.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.GuestBook;
+import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor // 의존성 자동 주입
+public class GuestBookServiceImpl implements GuestbookService{
+
+    private final GuestbookRepository repository; // 반드시 final로 선언
+    
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO-----------------------");
+        log.info(dto);
+
+        GuestBook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, GuestBook> getList(PageRequestDTO requestDTO){
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<GuestBook> result = repository.findAll(pageable);
+
+        Function<GuestBook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
+    }
+}
---
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -0,0 +1,39 @@
+package org.zerock.guestbook.service;
+
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.GuestBook;
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+
+    default GuestBook dtoToEntity(GuestbookDTO dto){
+        GuestBook entity = GuestBook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+
+    default GuestbookDTO entityToDto(GuestBook entity){
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+
+    PageResultDTO<GuestbookDTO, GuestBook> getList(PageRequestDTO requestDTO);
+}
+
+// Java 8버전부터는 인터페이스의 실제 내용을 가지는 코드를 default라는 키워드로 생성할 수 있다.
+// 이를 이용하면 기존에 추상 클래스를 통해서 전달해야 하는 실제 코드를 인터페이스에 선언할 수 있다.
+// 인터페이스 -> 추상클래스 -> 구현클래스의 형태로 구현되던 방식에서 추상 클래스를 생략하는 것이 가능
