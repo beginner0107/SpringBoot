4a68cc0b531cdf7d13d9605b77e525c0527eb9d1
Beginner <dkstmdwn17@naver.com>
2022-07-20 17:16:21 +0900

    HTTP 응답 - HTTP API, 메시지 바디에 직접 입력

--- springmvc/src/main/java/hello/springmvc/domain/HelloData.java
+++ springmvc/src/main/java/hello/springmvc/domain/HelloData.java
@@ -1,13 +1,19 @@
 package hello.springmvc.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /*
 @Data -> @Setter, @Getter, @ToString
          @EqualsAndHashCode, @RequiredArgsConstructor
          자동 적용
  */
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class HelloData {
     private String username;
     private int age;
+
 }
