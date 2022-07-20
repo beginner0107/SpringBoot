f5b5b1a792d077164bdbbc855ec8b785afac125c
Beginner <dkstmdwn17@naver.com>
2022-07-21 00:26:37 +0900

    상품 저장소 : ItemRepository

---
+++ item-service/item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,50 @@
+package hello.itemservice.domain.item;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    // 저장
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+    // 목록
+    public List<Item>findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    // 수정
+    public void update(Long itemId, Item updateParam){
+        Item item = findById(itemId);
+        item.setItemName(updateParam.getItemName());
+        item.setPrice(updateParam.getPrice());
+        item.setQuantity(updateParam.getQuantity());
+    }
+    // 삭제
+    public void remove(Long itemId){
+        store.remove(itemId);
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+    
+}
